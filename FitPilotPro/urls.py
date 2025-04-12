3a4418ce2defcfe19ec7ab7565394d1f57d9990a
Kashem Khondaker <kashem.khondaker.official001@gmail.com>
2025-04-12 09:17:24 +0600

    first commit

---
+++ FitPilotPro/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from debug_toolbar.toolbar import debug_toolbar_urls
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+] + debug_toolbar_urls()

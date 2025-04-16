5f7c2883436438ce3e7796b92fe7259df9d3ec38
Kashem Khondaker <kashem.khondaker.official001@gmail.com>
2025-04-16 18:04:39 +0600

    supabase database setup

--- FitPilotPro/settings.py
+++ FitPilotPro/settings.py
@@ -73,12 +73,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'FitPilotPro.wsgi.app'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('dbname'),
+        'USER': config('user'),
+        'PASSWORD': config('password'),
+        'HOST': config('host'),
+        'PORT': config('port'),
     }
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
